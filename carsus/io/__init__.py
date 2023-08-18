a1b935e6eeccc01f0584ff40481b78cec82fab5f
Shikha Rathi <admin@srathi.me>
2023-08-18 17:25:01 +0530

    modify init.py

--- carsus/io/__init__.py
+++ carsus/io/__init__.py
@@ -1,6 +1,6 @@
 import os
-from carsus.io.nist import NISTIonizationEnergiesParser, NISTWeightsCompPyparser
-     #NISTIonizationEnergiesIngester, NISTWeightsCompIngester
+from carsus.io.nist import NISTIonizationEnergiesParser, NISTIonizationEnergiesIngester,\
+    NISTWeightsCompPyparser, NISTWeightsCompIngester
 from carsus.io.kurucz import GFALLReader, GFALLIngester
 from carsus.io.output import AtomData
 from carsus.io.zeta import KnoxLongZetaIngester
